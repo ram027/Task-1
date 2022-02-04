2cbacd497e2ad3e1776451bd4d2c11c896077db7
ram027 <ram1218290@jmit.ac.in>
2022-02-04 14:54:55 +0530

    task2 generic

--- task2.js
+++ task2.js
@@ -1,15 +1,12 @@
-const tast2Obj = JSON.stringify(require('./task2.json'));
+const tast2Obj = require('./task2.json');
 const iCanPrintJson = (obj) => {
-    obj = JSON.parse(obj);
-    const { glossary } = obj;
-    const { title, GlossDiv } = glossary;
-    console.log(`Glossary title is ${title}`);
-    const { title:glossDivTitle, GlossList } = GlossDiv;
-    console.log(`GlossDiv title is ${glossDivTitle}`);
-    const { GlossEntry } = GlossList;
-    for (let el in GlossEntry) {
-        console.log(`GlossEntry ${el} is ${GlossEntry[el]}`);
+      for (const element in obj) {
+    if (typeof obj[element] === 'object'){ 
+      iCanPrintJson(obj[element]);
+    } else {
+      console.log(`key: ${element} : ${obj[element]}`);
     }
-   
+  }
 };
 iCanPrintJson(tast2Obj);
+
